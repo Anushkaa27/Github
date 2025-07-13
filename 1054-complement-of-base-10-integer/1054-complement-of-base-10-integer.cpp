adb235e3b3c238c24d2592f5417802ad491c40a4
Anushka Singh <80686533+Anushkaa27@users.noreply.github.com>
2025-07-13 15:36:19 +0530

    Time: 0 ms (100%), Space: 8 MB (23.38%) - LeetHub

--- 1054-complement-of-base-10-integer/1054-complement-of-base-10-integer.cpp
+++ 1054-complement-of-base-10-integer/1054-complement-of-base-10-integer.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     int bitwiseComplement(int n) {
-        int i = 0, number = 0, mask = 0;
-        // doesn't work for 0:
+        int i = n, number = 0, mask = 0;
+        // doesn't work for 0: edge case
         if(n ==0){
             return 1;
         }
-        while(n>0){
-            int bit = n&1;
-            if(bit==0){
-                number+=pow(2,i);
-            }
-            n=n>>1;
-            i++;
-        }
-        // while(n>0)
+        while(i>0){
+            //ANOTHER WAY TO SOLVE THIS:
+            mask = (mask<<1) | 1;
+            i = i >> 1;
 
-        return number;
+            // MY METHOD:
+            // int bit = n&1;
+            // if(bit==0){
+            //     number+=pow(2,i);
+            // }
+            // n=n>>1;
+            // i++;
+        }
+        return number = (mask & ~n);
     }
 };
