4cdb54c3e9c86b600efc53123d6c1e388d78210a
Anushka Singh <80686533+Anushkaa27@users.noreply.github.com>
2026-09-06 20:31:48 +0530

    Time: 0 ms (100%), Space: 7.8 MB (81.84%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,20 +1,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        // int ans = 1;
-        // for (int i = -31; i < 31; i++) {
-        //     // if(n== pow(2, i)){- instead of calculating the power each time
-        //     we
-        //     // chose to use the previous value of answer
-        //     if (n == ans) {
-        //         return true;
-        //         break;
-        //     }
-        //     if (ans < INT_MAX / 2) {//otherwise the code breaks for 2^30
-        //         ans *= 2;
-        //     }
-        // }
-        // ANOTHER APPROACH:
-        return n > 0 && not(n & (n - 1));
+        return n>0 && (n & (n-1)) == 0;
     }
 };
